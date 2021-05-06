451ab46bccd883c7b0e85964aa025e4071f04d73
Harish <36741511+harishbala1o@users.noreply.github.com>
2021-05-06 22:24:11 +0530

    Kommunicate script code updated

--- Sample-Angular-js-chat/src/app/app.component.ts
+++ Sample-Angular-js-chat/src/app/app.component.ts
@@ -8,12 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my first app';
   ngOnInit() {
-    (function(d, m){
-      var kommunicateSettings = {"appId":"kommunicate-support","conversationTitle":"Devashish"};
-      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-      (window as any).kommunicate = m; m._globals = kommunicateSettings;
-    })(document, (window as any).kommunicate || {});
+     (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"kommunicate-support","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
   }
 }
