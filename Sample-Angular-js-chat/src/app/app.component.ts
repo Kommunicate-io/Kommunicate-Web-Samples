afa3068fe8acbc4011222dec4c3d512e17ee4f58
Harish <36741511+harishbala1o@users.noreply.github.com>
2021-05-06 22:02:14 +0530

    Kommunicate source link updated to V2

--- Sample-Angular-js-chat/src/app/app.component.ts
+++ Sample-Angular-js-chat/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
     (function(d, m){
       var kommunicateSettings = {"appId":"kommunicate-support","conversationTitle":"Devashish"};
       var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-      s.src = "https://api.kommunicate.io/v2/kommunicate.app";
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
       var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
       (window as any).kommunicate = m; m._globals = kommunicateSettings;
     })(document, (window as any).kommunicate || {});
